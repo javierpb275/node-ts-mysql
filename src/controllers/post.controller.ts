6a44fb9b4385cca14a92d7179ab62fbae9f10923
javierpb275 <javierpb275@gmail.com>
2022-01-30 17:27:59 +0100

    getting updating and deleting post by id

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -11,6 +11,16 @@ export const getPosts = async (
   return res.json(posts[0]);
 };
 
+export const getPost = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const id = req.params.id;
+  const conn = await connect();
+  const posts = await conn.query(`SELECT * FROM posts WHERE id = ?`, [id]);
+  return res.json(posts[0]);
+};
+
 export const createPost = async (
   req: Request,
   res: Response
@@ -19,4 +29,25 @@ export const createPost = async (
   const conn = await connect();
   await conn.query(`INSERT INTO posts SET ?`, [newPost]);
   return res.json({ message: "Post created" });
+};
+
+export const deletePost = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const id = req.params.id;
+  const conn = await connect();
+  await conn.query(`DELETE FROM posts WHERE id = ?`, [id]);
+  return res.json({ message: "Post deleted" });
+};
+
+export const updatePost = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const id = req.params.id;
+  const updatedPost: Post = req.body;
+  const conn = await connect();
+  await conn.query(`UPDATE posts set ? WHERE id = ?`, [updatedPost, id]);
+  return res.json({ message: "Post updated" });
 };
--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { createPost, getPosts } from "../controllers/post.controller";
+import {
+  createPost,
+  getPosts,
+  getPost,
+  deletePost,
+  updatePost,
+} from "../controllers/post.controller";
 
 const router = Router();
 
 router.route("/").get(getPosts).post(createPost);
+
+router.route("/:id").get(getPost).delete(deletePost).put(updatePost);
 
 export default router;
